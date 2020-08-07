d84a4372e87af411725eba6d6139d3ee3348c58b
Andrew Hickman <andrew.hickman1@sky.com>
2020-08-07 20:05:27 +0100

    Move ObjectDatabase to its own module

--- src/object.rs
+++ src/object.rs
@@ -1,12 +1,11 @@
+mod database;
+
+pub use self::database::ObjectDatabase;
+
 use std::fmt;
-use std::fs::OpenOptions;
-use std::io::{self, Write as _};
-use std::path::PathBuf;
+use std::io;
 use std::str::FromStr;
 
-use flate2::read::ZlibDecoder;
-use flate2::write::ZlibEncoder;
-use flate2::Compression;
 use hex::FromHex;
 use sha1::digest::Digest;
 use sha1::Sha1;
@@ -15,11 +14,6 @@ use thiserror::Error;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Id([u8; 20]);
-
-#[derive(Debug)]
-pub struct ObjectDatabase {
-    path: PathBuf,
-}
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -54,55 +48,6 @@ impl Id {
     pub fn to_hex(&self) -> String {
         hex::encode(&self.0)
     }
-}
-
-impl ObjectDatabase {
-    pub fn open(path: impl Into<PathBuf>) -> Self {
-        ObjectDatabase { path: path.into() }
-    }
-
-    pub fn read_object(&self, id: &Id) -> Result<impl io::Read, Error> {
-        let hex = id.to_hex();
-        let (dir, file) = object_path_parts(&hex);
-        let mut path = self.path.join(dir);
-        path.push(file);
-
-        match fs_err::File::open(path) {
-            Ok(file) => Ok(ZlibDecoder::new(file)),
-            Err(err) if err.kind() == io::ErrorKind::NotFound => {
-                Err(Error::ObjectNotFound(Box::new(*id)))
-            }
-            Err(err) => Err(err.into()),
-        }
-    }
-
-    pub fn write_object(&self, bytes: &[u8]) -> Result<(), Error> {
-        let id = Id::from_hash(bytes);
-        let hex = id.to_hex();
-        let (dir, file) = object_path_parts(&hex);
-
-        let mut path = self.path.join(dir);
-        match fs_err::create_dir(&path) {
-            Err(err) if err.kind() != io::ErrorKind::AlreadyExists => return Err(err.into()),
-            _ => (),
-        }
-
-        path.push(file);
-        let file = match OpenOptions::new().create_new(true).write(true).open(&path) {
-            Ok(file) => fs_err::File::from_parts(file, path),
-            Err(err) if err.kind() == io::ErrorKind::AlreadyExists => return Ok(()),
-            Err(err) => return Err(err.into()),
-        };
-
-        let mut encoder = ZlibEncoder::new(file, Compression::best());
-        encoder.write_all(bytes)?;
-        encoder.finish()?;
-        Ok(())
-    }
-}
-
-fn object_path_parts(hex: &str) -> (&str, &str) {
-    hex.split_at(2)
 }
 
 impl fmt::Display for Id {
